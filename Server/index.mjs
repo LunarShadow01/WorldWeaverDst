08678cbc691f7521245b5164ce4cfe7ae9ff0f5e
LunarShadow <gtavgtav277@gmail.com>
2025-03-26 17:39:51 +0200

    Revert "attempt to move game files location"
    This reverts commit 5a7ca94bb2fcce01adde628e28217cce9308ba7e.

--- Server/index.mjs
+++ Server/index.mjs
@@ -2,19 +2,15 @@ import path from "node:path";
 import { Cluster } from "./server_objects.mjs";
 
 export const dirname = path.dirname(import.meta.url)
+// const dir = "/mnt/c/"
 const dir = "C:/"
-const game_files_dir_name = "game_files"
-const clusters_dir_name = "clusters"
-
 const game_dir = dir+"WorkingSpace/Software/Steam/steamapps/common/Don't Starve Together Dedicated Server"
-// const game_dir = path.resolve(dirname, game_files_dir_name)
-const clusters_dir = path.resolve(dirname, clusters_dir_name)
-const cluster_name = "Cluster_test"
+const cluster_dir = "Cluster_test"
 const cluster_token = "pds-g^KU_dwt6dsdZ^+o+0rGzyzVvBOihj4Howjh+Z51zfOU7n2B60QescaY4="
 
 function main() {
   const cluster = new Cluster()
-  .setup(["Master"], game_dir, clusters_dir, cluster_name, cluster_token)
+  .setup(["Master"], game_dir, cluster_dir, cluster_token)
 
   cluster.start()
 
