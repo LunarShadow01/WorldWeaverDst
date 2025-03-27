cc249f85c1cdfcff6e89cdb05c419ff6ae4f2667
LunarShadow <gtavgtav277@gmail.com>
2025-03-27 20:16:51 +0200

    fixed tailwindcss intellisense
    i just moved the config to the project root...

--- world-weaver/src/App.jsx
+++ world-weaver/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
 
   return (
-    <div className="">
+    <div className=''>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
