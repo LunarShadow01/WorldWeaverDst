623ebfcc5a3677340381b87c403f89e1989cf660
LunarShadow <gtavgtav277@gmail.com>
2025-03-28 09:03:03 +0300

    SelectField improvements
    select field will match the width
    of the longest option label given
    so that all labels can fit in the drop down
    menu nicely
    
    this might be subject to change but it looks
    better for now

--- world-weaver/src/components/SelectField.jsx
+++ world-weaver/src/components/SelectField.jsx
@@ -8,6 +8,23 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react'
  * @returns {React.JSX.Element}
  */
 export default function SelectField({options=[], label="undefined", setValue=null}) {
+  /**@type {String} */
+  const longest_memo = useMemo(() => {
+    /**@type {String} */
+    let longest_label = ""
+    if (options.length > 0) {
+      longest_label = options[0].label
+    }
+
+    for (const option of options) {
+      if (option.label.length > longest_label.length) {
+        longest_label = option.label
+      }
+    }
+
+    return longest_label
+  }, [options])
+
   const selected_memo = useMemo(() => {
     let selected_opt = {label: "", value: -1}
     if (options.length > 0) {
@@ -16,7 +33,7 @@ export default function SelectField({options=[], label="undefined", setValue=nul
     
     for (const i in options) {
       let option = options[i]
-      if ("selected" in option) {
+      if ("selected" in option && option.selected) {
         selected_opt = option
         break
       }
@@ -39,19 +56,23 @@ export default function SelectField({options=[], label="undefined", setValue=nul
 
   return (
     <div className='flex justify-center items-start w-fit'>
-      <div className='grid grid-cols-2 justify-center items-center gap-1'>
-        <label className='h-full text-text bg-secondary p-1 rounded-l-lg
+      <div className='flex justify-center items-center gap-1'>
+        <label className='h-full text-text bg-secondary p-2 rounded-l-lg
         flex items-center justify-center'>{label}:</label>
         <div className='flex h-full rounded-r-lg bg-secondary
           text-text p-2 relative'
           onClick={() => {setIsOpen(!is_open)}}>
           <div className='flex justify-between items-center w-full gap-x-2'>
-            <div>{cur_label}</div>
+            <div className='grid grid-cols-1 grid-rows-1 items-center justify-center'>
+              <div className='opacity-0 col-start-1 row-start-1'>{longest_memo}</div>
+              <div className='col-start-1 row-start-1'>{cur_label}</div>
+            </div>
             <div className={`${is_open ? "rotate-90" : ""} transition-all duration-150`}>
               {">"}
             </div>
           </div>
-          <div className={`${is_open ? "" : "hidden"} absolute top-0 left-0 translate-y-1/3 w-full h-fit bg-secondary rounded-lg`}>
+          <div className={`${is_open ? "" : "hidden"} absolute top-0 left-0 translate-y-1/3
+            w-full h-fit bg-secondary rounded-lg`}>
             <div className='flex flex-col grow w-full justify-start items-center gap-2 p-2'>
               {options.map((elm, i) => {
                 return (
--- world-weaver/src/pages/Components.jsx
+++ world-weaver/src/pages/Components.jsx
@@ -10,7 +10,7 @@ export default function Components() {
       <Button><div className='w-8 aspect-square bg-text rounded-lg'></div></Button>
       <SelectField label='test options' options={[
           {label: "test 1", value: 0, selected: false},
-          {label: "test 2", value: 1, selected: undefined},
+          {label: "test 2 with extra text", value: 1, selected: undefined},
           {label: "test 3", value: 2, selected: true},
           {label: "test 4", value: 3},
         ]}></SelectField>
